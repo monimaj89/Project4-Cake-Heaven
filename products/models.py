68978103f9d52eac76a155422d510949ac4c0a05
monimaj89 <mn.majchrowicz@gmail.com>
2023-10-28 11:28:44 +0000

    Add models for categories and products

--- products/models.py
+++ products/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+#  Category Model
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        """ String representation of Category name """
+        return self.name
+
+    def get_friendly_name(self):
+        """ Model Method to return friendly_name """
+        return self.friendly_name
+
+
+# Products Model
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+        )
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    ingredients = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+        blank=True
+        )
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        """ String representation of Product name """
+        return self.name
