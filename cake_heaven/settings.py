1f71727817c135d01d7dcd27ceed286e0a79ad7d
monimaj89 <mn.majchrowicz@gmail.com>
2023-11-08 11:56:07 +0000

    Remove secret key and set debug

--- cake_heaven/settings.py
+++ cake_heaven/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-83+h^-*-4-q@fgj41qvsgwy3=m-62wijyk+!(l)e)msv97jf(k'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-monimaj89-project4cakeh-m2z134cnh2w.ws-eu105.gitpod.io',
