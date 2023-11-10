1fa463cd6ce954eea0156d6b3b54fde19c351945
monimaj89 <mn.majchrowicz@gmail.com>
2023-11-10 14:05:24 +0000

    Add sort functionality to sort products

---
+++ static/js/sort_box.js
@@ -0,0 +1,21 @@
+// Sort Products Box
+// Adapted from Code Institute's Boutique Ado Walkthrough
+
+$('#sort-selector').change(function(){
+    var selector = $(this);
+    var currentUrl = new URL(window.location);
+
+    var selectedVal = selector.val();
+    if(selectedVal != "reset"){
+        var sort = selectedVal.split("_")[0];
+        var direction = selectedVal.split("_")[1];
+
+        currentUrl.searchParams.set("sort", sort);
+        currentUrl.searchParams.set("direction", direction);
+
+        window.location.replace(currentUrl);
+    }else{
+        currentUrl.searchParams.delete("sort");
+        currentUrl.searchParams.delete("direction");
+    }
+});
