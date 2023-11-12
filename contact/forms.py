6c13eef5ccecd5d3fa04af0ad13d30f5abe4886c
monimaj89 <mn.majchrowicz@gmail.com>
2023-11-12 15:02:01 +0000

    Add contact forms and adjust contact models

---
+++ contact/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    """
+    Form for Contact Model
+    """
+
+    class Meta:
+        model = Contact
+        fields = ('name', 'email', 'subject', 'description')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+     
+        # Sets placeholder on inputs
+        self.fields['name'].widget.attrs['placeholder'] = 'Your Name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Your Email'
+        self.fields['subject'].widget.attrs['placeholder'] = 'Message Subject'
+        self.fields['description'].widget.attrs['placeholder'] = 'Your Message'
+
+        for field in self.fields:
+            # Adds stylings classes to inputs
+            self.fields[field].widget.attrs['class'] = (
+                'mb-3 px-2 py-2 rounded-0 shadow-sm text-black')
--- contact/models.py
+++ contact/models.py
@@ -7,8 +7,8 @@ class Contact(models.Model):
     name = models.CharField(max_length=100, null=True, blank=False)
     email = models.EmailField(max_length=250, null=True, blank=False)
     subject = models.CharField(max_length=40, null=False, blank=False)
-    description = models.TextField(max_length=1000)
-    date = models.DateField()
+    description = models.TextField()
+    data_sent = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.email
