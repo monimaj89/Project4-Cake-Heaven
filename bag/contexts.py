612291020a28c2643dcb31742104a00d75157eaf
monimaj89 <mn.majchrowicz@gmail.com>
2023-11-17 17:50:57 +0000

    Update shopping bag template

--- bag/contexts.py
+++ bag/contexts.py
@@ -21,7 +21,7 @@ def bag_contents(request):
             'product': product,
         })
 
-    if total:
+    if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
