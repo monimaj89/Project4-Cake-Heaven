336c951ab558d3d7ed4255cb161f7080b242c7bc
monimaj89 <mn.majchrowicz@gmail.com>
2023-11-23 13:01:42 +0000

    Add basic checkout functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,9 +6,9 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePubliKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePubliKey);
 var elements = stripe.elements();
 var style = {
     base: {
@@ -27,3 +27,64 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+// Event listener for payment form submit
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+
+    // Disables the submit button & card input to prevent multiple submission
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+
+    // Passes card details to Stripe
+    stripe.confirmCardPayment(clientSecret, {
+
+        // Sends payment details 
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+
+        // Send error message in case of error occurs
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+
+            // Re-enable the submit button and card input
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+            
+            // if payment details are valid submit payment form
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
