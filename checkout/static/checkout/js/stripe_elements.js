9207043f9812672afca406545b7b39765ad979cd
monimaj89 <mn.majchrowicz@gmail.com>
2023-11-24 16:44:09 +0000

    Add checkout form data caching in payment intent

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -63,40 +63,77 @@ form.addEventListener('submit', function(ev) {
     // Loading animation
     $('#loading-overlay').fadeToggle(100);
 
-    // Passes card details to Stripe
-    stripe.confirmCardPayment(clientSecret, {
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    // From using {% csrf_token %} in the form
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    var url = '/checkout/cache_checkout_data/';
 
-        // Sends payment details 
-        payment_method: {
-            card: card,
-        }
-    }).then(function(result) {
+    $.post(url, postData).done(function(){
+        // Passes card details to Stripe
+        stripe.confirmCardPayment(clientSecret, {
 
-        // Send error message in case of error occurs
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
-            var html = `
-                <span role="alert">
-                <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>`;
-            $(errorDiv).html(html);
+            // Sends payment details 
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
+                    address:{
+                        line1: $.trim(form.street_address1.value),
+                        line2: $.trim(form.street_address2.value),
+                        city: $.trim(form.town_or_city.value),
+                        country: $.trim(form.country.value),
+                    }
+                }
+            },
+            shipping: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    postal_code: $.trim(form.postcode.value),
+                }
+            },
+        }).then(function(result) {
 
-            // Reload the payment form
-            $('#payment-form').fadeToggle(100);
+            // Send error message in case of error occurs
+            if (result.error) {
+                var errorDiv = document.getElementById('card-errors');
+                var html = `
+                    <span role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+                $(errorDiv).html(html);
 
-            // Hides the loading animation
-            $('#loading-overlay').fadeToggle(100);
+                // Reload the payment form
+                $('#payment-form').fadeToggle(100);
 
-            // Re-enable the submit button and card input
-            card.update({ 'disabled': false});
-            $('#submit-button').attr('disabled', false);
-            
-            // if payment details are valid submit payment form
-        } else {
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
+                // Hides the loading animation
+                $('#loading-overlay').fadeToggle(100);
+
+                // Re-enable the submit button and card input
+                card.update({ 'disabled': false});
+                $('#submit-button').attr('disabled', false);
+                
+                // if payment details are valid submit payment form
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                }
             }
-        }
-    });
+        });
+    }).fail(function () {
+        // just reload the page, the error will be in django messages
+        location.reload();
+    })
 });
