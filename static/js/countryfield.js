18ff4797540cc657b1784160bb8bf217e37ac666
monimaj89 <mn.majchrowicz@gmail.com>
2023-11-25 14:19:22 +0000

    Update profile form styling with js

---
+++ static/js/countryfield.js
@@ -0,0 +1,15 @@
+// Country input styling to display previously selected country
+// Adapted from Code Institute's Boutique Ado Walkthrough
+
+let countrySelected = $('#id_default_country').val();
+if(!countrySelected) {
+    $('#id_default_country').css('color', '#aab7c4');
+};
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#aab7c4');
+    } else {
+        $(this).css('color', '#000');
+    }
+});
